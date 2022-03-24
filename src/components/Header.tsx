f4023201b9b356ce1ff6bbd67f6f149417f589d1
Paulo Spiguel <paulospil@hotmail.com>
2022-03-24 18:17:28 +0000

    add avatar header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
-  return <header>Paulo Spiguel</header>;
+  return (
+    <header className="w-full ">
+      <div className="flex items-center space-x-2">
+        <Image
+          width="60px"
+          height="60px"
+          className="rounded-full"
+          src="/assets/avatarProfile_BW.webp"
+          alt="Homem branco, soridente, de meia idade, de aproximadamente 36 anos, 
+          cabelos curtos, crespo e escuros, olhos castanhos de barbas escuras de tamanho média, 
+          vestindo um t-shert azul."
+        />
+        <div className="flex flex-col">
+          <span className="uppercase font-extrabold text-gray-800">Paulo Spiguel</span>
+          <span className="text-gray-600">Software Engineer</span>
+        </div>
+      </div>
+      <nav></nav>
+    </header>
+  );
 }
